a254d0be5a2b8373f851d473b035239def29ab30
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-05-27 17:50:38 +0100

    Main Window Creation

--- main.py
+++ main.py
@@ -9,4 +9,22 @@ main_window.configure(bg=settings.MAIN_COLOR)
 main_window.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 main_window.resizable(False, False)
 
+btnStart = Button(
+    main_window,
+    width=20,
+    height=3,
+    text='START'
+)
+
+btnStart.place(x=50, y=50)
+
+btnExit = Button(
+    main_window,
+    width=20,
+    height=3,
+    text='EXIT'
+)
+
+btnExit.place(x=50, y=120)
+
 main_window.mainloop()
