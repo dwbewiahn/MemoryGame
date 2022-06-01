e4afd11c5bf5d2b87a5bdd3e49521d021ce86b11
Douglas Bewiahn <dwbewiahn@gmail.com>
2022-06-01 22:18:36 +0100

    Game Window connected with main window.

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ main_window = Tk()
 # Main window Settings
 main_window.title('Memory Card Game')
 main_window.configure(bg=settings.MAIN_COLOR)
-main_window.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+main_window.geometry('600x600')
 main_window.resizable(False, False)
 
 btnStart = Button(
